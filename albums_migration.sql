8bdac82ddf747e38e439216ccf791a053a69c4c2
ciaorachel <ciaorachel@gmail.com>
2015-07-28 15:11:01 -0500

    created albums migration for exercise beginning mysql

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date YEAR(4) NOT NULL,
+    sales DECIMAL(12,2) NOT NULL,
+    genre VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id)
+);
