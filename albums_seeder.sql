193556187272afb312c088bc04c4ea4721b6ade6
ciaorachel <ciaorachel@gmail.com>
2015-07-28 15:11:39 -0500

    created albums seeder file for inserting data into my albums table

---
+++ albums_seeder.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 42.4, 'pop, rock, R&B'),
+('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 32.2, 'Rock, soft rock, folk rock'),
+('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock'),
+('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'Soundtrack/R&B, soul, pop'),
+('AC/DC', 'Back in Black', 1980, 25.9, 'Hard rock');
