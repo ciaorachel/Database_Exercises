3b2dd5720f4affbbcb709123b05dc4fcabf5b6f9
ciaorachel <ciaorachel@gmail.com>
2015-07-29 12:58:36 -0500

    completed where exercises for beginning mysql

---
+++ where_exercises.sql
@@ -0,0 +1,20 @@
+USE employees;
+
+SELECT first_name, last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+/*Employees with first names 'Irena', 'Vidya', or 'Maya'. Output: 709 rows (Hint: Use IN).*/
+
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%';
+/*Employees whose last name starts with 'E'. Output: 7,330 rows.*/
+
+
+SELECT first_name, last_name, hire_date FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+/*Employees hired in the 90s — 135,214 rows.*/
+
+
+SELECT first_name, last_name, birth_date FROM employees WHERE birth_date LIKE '%-12-25';
+/*Employees born on Christmas — 842 rows.*/
+
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%';
+/*Employees with a 'q' in their last name — 1,873 rows.*/
