62d909698832ad3cdcd31ece2413b1fb32d29f15
ciaorachel <ciaorachel@gmail.com>
2015-07-29 14:48:03 -0500

    this is final version of updated order_by_exercises.spf file - i added/commited in each step on the spf file, this is just to see the final ending version

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,46 +1,50 @@
 USE employees;
 
 SELECT concat(first_name,' ', last_name) AS full_name 
-FROM employees 
+FROM employees
 WHERE first_name = 'Irena' 
 	OR first_name = 'Vidya' 
 	OR first_name ='Maya'
-	AND gender = 'M';
-/*Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN. Output: 709 rows*/
+	AND gender = 'M'
+ORDER BY last_name DESC;
+/*Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN. Order by last name descending. Your last result should still be Irena Acton but now the first result should be Maya Zyda. Output: 709 rows*/
 
 
-SELECT concat(first_name,' ', last_name) AS full_name, gender
+SELECT emp_no, concat(first_name,' ', last_name) AS full_name, gender
 FROM employees 
 WHERE gender = 'M'
 AND (
 	first_name = 'Irena' 
 	OR first_name = 'Vidya' 
 	OR first_name ='Maya'
-);
-/*Add a condition to find everybody with those names who is also male. Output: 441 rows.*/
+)
+ORDER BY emp_no DESC;
+/*Add a condition to find everybody with those names who is also male. Sort the results by their employee number descending. Results do not change. Output: 441 rows.*/
 
 
-SELECT first_name, last_name 
+SELECT emp_no, first_name, last_name 
 FROM employees 
 WHERE last_name LIKE 'E%' 
-	OR last_name LIKE '%e';
-/*Find all employees whose last name starts or ends with 'E'. Output: 30,723 rows.*/
+	OR last_name LIKE '%e'
+ORDER BY emp_no DESC;
+/*Find all employees whose last name starts or ends with 'E'. Sort the results descending by their employee number - this reverses order. Output: 30,723 rows.*/
 
 
-SELECT first_name, last_name 
+SELECT emp_no, first_name, last_name 
 FROM employees 
 WHERE last_name LIKE 'E%' 
-	AND last_name LIKE '%e';
-/*Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E'. Output: 899 rows.*/
+	AND last_name LIKE '%e'
+ORDER BY emp_no DESC;
+/*Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E'. Sort the results descending by their employee number - this reverses order. Output: 899 rows.*/
 
 
-SELECT first_name, last_name, hire_date 
+SELECT first_name, last_name, hire_date, birth_date 
 FROM employees 
 WHERE hire_date BETWEEN '1990-01-01' 
 	AND '1999-12-31'
-	AND birth_date LIKE '%-12-25';
-/*Find all employees hired in the 90s and born on Christmas. Output: 362 rows.*/
-
+	AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+/*Find all employees hired in the 90s and born on Christmas. Change the query such that the first result is the oldest employee who was hired last. It should be Khun Bernini. Output: 362 rows.*/
 
 
 SELECT first_name, last_name 
