b3e151cef5054215d96d4501fd56116cfccdcebd
ciaorachel <ciaorachel@gmail.com>
2015-07-30 11:07:35 -0500

    cut extra spaces

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -23,7 +23,7 @@ SELECT emp_no, concat(first_name, ' ', last_name) AS full_name
 FROM employees 
 WHERE last_name LIKE 'E%' 
 	AND last_name LIKE '%e'
-ORDER BY emp_no DESC ;
+ORDER BY emp_no DESC;
 
 
 
@@ -33,7 +33,7 @@ FROM employees
 WHERE hire_date BETWEEN '1990-01-01' 
 	AND '1999-12-31'
 	AND birth_date LIKE '%-12-25'
-ORDER BY birth_date ASC, hire_date DESC ;
+ORDER BY birth_date ASC, hire_date DESC;
 
 
 
