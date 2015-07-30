dcf37b009adb7e5ea18439b4659f4230eeb67cd3
ciaorachel <ciaorachel@gmail.com>
2015-07-30 10:40:03 -0500

    updated queries for employees whose names start and/or end with E. concatted first and last names into single column called full name

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -8,32 +8,23 @@ WHERE first_name = 'Irena'
 GROUP BY gender;
 
 
-SELECT emp_no, concat(first_name,' ', last_name) AS full_name, gender
-FROM employees 
-WHERE gender = 'M'
-AND (
-	first_name = 'Irena' 
-	OR first_name = 'Vidya' 
-	OR first_name ='Maya'
-)
-ORDER BY emp_no DESC;
-/*Add a condition to find everybody with those names who is also male. Sort the results by their employee number descending. Results do not change. Output: 441 rows.*/
 
-
-SELECT emp_no, first_name, last_name 
+SELECT emp_no, concat(first_name, ' ', last_name) AS full_name
 FROM employees 
 WHERE last_name LIKE 'E%' 
 	OR last_name LIKE '%e'
 ORDER BY emp_no DESC;
-/*Find all employees whose last name starts or ends with 'E'. Sort the results descending by their employee number - this reverses order. Output: 30,723 rows.*/
 
 
-SELECT emp_no, first_name, last_name 
+
+
+SELECT emp_no, concat(first_name, ' ', last_name) AS full_name 
 FROM employees 
 WHERE last_name LIKE 'E%' 
 	AND last_name LIKE '%e'
 ORDER BY emp_no DESC;
-/*Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E'. Sort the results descending by their employee number - this reverses order. Output: 899 rows.*/
+
+
 
 
 SELECT first_name, last_name, hire_date, birth_date 
