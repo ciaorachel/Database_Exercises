0cd65c6840b0f2c7216ea7646959d5ed50e9812e
ciaorachel <ciaorachel@gmail.com>
2015-07-30 13:59:58 -0500

    added db name as alias to second query

--- aliases.sql
+++ aliases.sql
@@ -4,3 +4,7 @@ SELECT concat(emp_no,' - ',last_name,', ',first_name) AS full_name, birth_date A
 FROM employees
 ORDER BY emp_no
 LIMIT 10;
+
+
+SELECT concat(e.first_name, ' ', e.last_name) AS full_name, e.birth_date AS DOB FROM employees AS e
+LIMIT 5;
