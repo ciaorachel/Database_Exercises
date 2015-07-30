f2bba511c344e0776afc57a71962130f62997677
ciaorachel <ciaorachel@gmail.com>
2015-07-30 11:06:46 -0500

    cleaned up file to remove my notes on query and results for last query

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -37,7 +37,6 @@ ORDER BY birth_date ASC, hire_date DESC ;
 
 
 
-/* Add a GROUP BY clause to your query for last names with 'q' and not 'qu' to find the distinct combination of first and last names. You will find there were some duplicate first and last name pairs in your previous results. Add a count() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.*/
 
 SELECT COUNT(*) AS count, concat(first_name, ' ', last_name) AS full_name
 FROM employees 
@@ -45,9 +44,3 @@ WHERE last_name LIKE '%q%'
 	AND last_name NOT LIKE '%qu%'
 GROUP BY full_name
 ORDER BY count DESC, last_name, first_name;
-
-/* (This QUERY below verifies my results FROM previous QUERY)*/
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE first_name = 'Constantino'
-AND last_name = 'Chleq';
