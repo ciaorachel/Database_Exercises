a58e870bfd77a1119be3263559ae6fed755fc54d
ciaorachel <ciaorachel@gmail.com>
2015-07-30 09:28:06 -0500

    updated with query to find unique combos of first and last names. output is 846 rows.

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -4,8 +4,8 @@ SELECT DISTINCT title
 FROM titles
 ORDER BY title;
 
-SELECT last_name 
+SELECT concat(first_name, ' ', last_name) AS full_name 
 FROM employees 
 WHERE last_name LIKE 'E%' 
 	AND last_name LIKE '%e'
-GROUP BY last_name;
+GROUP BY first_name, last_name;
