2b93013209384e76e170b2aa1254df684064967e
ciaorachel <ciaorachel@gmail.com>
2015-07-30 10:48:42 -0500

    updated query for employees born on Christmas and hired in the 90s, and used datediff() to find how many days they have been working at the company

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -22,18 +22,20 @@ SELECT emp_no, concat(first_name, ' ', last_name) AS full_name
 FROM employees 
 WHERE last_name LIKE 'E%' 
 	AND last_name LIKE '%e'
-ORDER BY emp_no DESC;
+ORDER BY emp_no DESC ;
 
 
 
 
-SELECT first_name, last_name, hire_date, birth_date 
+SELECT first_name, last_name, birth_date, hire_date, DATEDIFF(now(), hire_date) AS how_many_days_working_since_hire
 FROM employees 
 WHERE hire_date BETWEEN '1990-01-01' 
 	AND '1999-12-31'
 	AND birth_date LIKE '%-12-25'
-ORDER BY birth_date ASC, hire_date DESC;
-/*Find all employees hired in the 90s and born on Christmas. Change the query such that the first result is the oldest employee who was hired last. It should be Khun Bernini. Output: 362 rows.*/
+ORDER BY birth_date ASC, hire_date DESC ;
+
+
+
 
 
 SELECT first_name, last_name 
