51ddd24134c954482df0a1f95d5a749126eb9035
ciaorachel <ciaorachel@gmail.com>
2015-07-30 14:02:49 -0500

    described each table in employees db and inspected keys/indices, then described albums table on codeup_test_db and unique index for album and artist name

---
+++ indices.sql
@@ -0,0 +1,18 @@
+USE employees;
+
+DESCRIBE employees.departments;
+
+DESCRIBE employees.dept_emp;
+
+DESCRIBE employees.dept_manager;
+
+DESCRIBE employees.employees;
+
+DESCRIBE employees.salaries;
+
+DESCRIBE employees.titles;
+
+
+USE codeup_test_db;
+
+DESCRIBE albums;
