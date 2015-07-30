ff1cdc019c5daf2ef5343080e4bddaf7a10ee10f
ciaorachel <ciaorachel@gmail.com>
2015-07-30 09:34:47 -0500

    added query for unique last names with a Q and not QU using distinct

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -6,6 +6,10 @@ ORDER BY title;
 
 SELECT concat(first_name, ' ', last_name) AS full_name 
 FROM employees 
-WHERE last_name LIKE 'E%' 
-	AND last_name LIKE '%e'
+WHERE last_name LIKE 'E%e' 
 GROUP BY first_name, last_name;
+
+SELECT DISTINCT last_name 
+FROM employees 
+WHERE last_name LIKE '%q%'
+	AND last_name NOT LIKE '%qu%';
