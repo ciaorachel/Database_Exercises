8932b3195873130013177fa4e311932b547c7e6d
ciaorachel <ciaorachel@gmail.com>
2015-07-30 11:56:31 -0500

    tightened up queries

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -21,14 +21,13 @@ ORDER BY emp_no DESC;
 
 SELECT emp_no, concat(first_name, ' ', last_name) AS full_name 
 FROM employees 
-WHERE last_name LIKE 'E%' 
-	AND last_name LIKE '%e'
+WHERE last_name LIKE 'E%e'
 ORDER BY emp_no DESC;
 
 
 
 
-SELECT first_name, last_name, birth_date, hire_date, DATEDIFF(now(), hire_date) AS how_many_days_working_since_hire
+SELECT concat(first_name, ' ', last_name) AS full_name, birth_date, hire_date, DATEDIFF(now(), hire_date) AS days_since_hire
 FROM employees 
 WHERE hire_date BETWEEN '1990-01-01' 
 	AND '1999-12-31'
@@ -38,9 +37,9 @@ ORDER BY birth_date ASC, hire_date DESC;
 
 
 
-SELECT COUNT(*) AS count, concat(first_name, ' ', last_name) AS full_name
+SELECT COUNT(*) AS how_many, concat(first_name, ' ', last_name) AS full_name
 FROM employees 
 WHERE last_name LIKE '%q%'
 	AND last_name NOT LIKE '%qu%'
 GROUP BY full_name
-ORDER BY count DESC, last_name, first_name;
+ORDER BY how_many DESC, last_name, first_name;
